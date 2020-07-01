63ba092868cb4f7cdf511a173ec0136f029fb01e
itsmsBrenda <67168084+itsmsBrenda@users.noreply.github.com>
2020-06-30 20:11:52 -0500

    Update inicio.blade.php

--- resources/views/Sitioweb/inicio.blade.php
+++ resources/views/Sitioweb/inicio.blade.php
@@ -5,55 +5,50 @@
                     <img src="{{asset('assets/img/1080x360-01.jpg')}}" alt="Image" class="img-fluid tm-welcome-img">
                 </div>
                 <div class="row mb-5">
-                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center mb-md-0 mb-4"><img src="{{asset('assets/img/575x400-01.jpg')}}" alt="Image" class="img-fluid"></div>
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center mb-md-0 mb-4"><img src="{{asset('assets/img/4.jpg')}}" alt="Image" class="img-fluid" width="450" ></div>
                     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 pl-md-4 tm-flex-center">
                         <div class="tm-about-text tm-flexbox-ie-fix">
-                            <h2 class="tm-orange-text mb-4">In sodales lorem nec metus</h2>
-                            <p class="mb-4">Ut ultrices, urna a porttitor condimentum, tortor arcu feugiat neque, quis tristique lectus nisl at nisi. In convallis, lorem eget bibendum lobortis, sem metus elementum odio, vitae mattis erat quam eu ante. Duis fermentum in tellus ut tempor. In hac habitasse platea dictumst. Nunc vel nulla lacus.</p>
-                            <a href="#" class="tm-button tm-button-normal tm-button-gray">Read More</a>    
+                            <h2 class="tm-orange-text mb-4">Acerca de Nosotros</h2>
+                            <p class="mb-4" style=”text-align: justify;” >
+                                <h3 class="tm-orange-text mb-4">MISIÓN</h3>
+                            Somos una empresa dedicada a ofrecer servicios de fotografía profesional,utilizando altos estándares de calidad en cada uno de nuestros productos y servicios, con el propósito de satisfacer las necesidades de nuestros clientes, ofreciendo un trato personalizado.</p>    
                         </div>                            
                     </div>
                 </div> <!-- row -->
                 <div class="row mb-5">
                     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 mb-md-0 mb-4 tm-flex-center">
                         <div class="tm-about-text tm-flexbox-ie-fix">
-                            <h2 class="tm-orange-text mb-4">Nullam condimentum purus</h2>
-                            <p class="mb-4">Curabitur consectetur sapien et vestibulum commodo. Phasellus id metus quam. Mauris fringilla arcu non tellus malesuada tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-                            <p>Donec commodo semper nisl semper luctus. Phasellus euismod, nulla eu tristique sodales, dolor risus lobortis eros, pretium hendrerit ligula risus sit amet odio.</p>
+                            <h2 class="tm-orange-text mb-4">VISIÓN</h2>
+                            <p class="mb-4" style=”text-align: justify;” >Ser la empresa líder en fotografía profesional, ofrecer amplios servicios y mayor cobertura a nivel nacional, utilizando para ello tecnologías innovadoras y un contacto más directo con nuestros clientes.</p>
                         </div>                            
                     </div>
-                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center pl-md-4"><img src="{{asset('assets/img/575x400-02.jpg')}}" alt="Image" class="img-fluid"></div>
-                </div> <!-- row -->
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center pl-md-4"><img src="{{asset('assets/img/5.jpg')}}" alt="Image" class="img-fluid" width="450" ></div>
+                </div> 
+                <h2 class="tm-orange-text mb-4">SERVICIOS</h2><!-- row -->
                 <div class="row mb-4">
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 mb-4">
                         <div class="tm-bg-orange p-5">
-                            <i class="fa fa-4x fa-audio-description d-block mb-4 text-center"></i>
-                            <h3 class="mb-4">Nullam condi mentum</h3>
-                            <p>Mauris fringilla arcu non tellus malesuada tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                            <img src="{{asset('assets/img/2.jpg')}}" alt="Image" class="img-fluid" width="150" align="text-center" >
+                            <h3 class="mb-4">XV AÑOS</h3>
+                            <p style=”text-align: justify;”  >Queremos que tengas las mejores fotografías de tu evento tan importante, por eso nos esforzamos para que conserves esos recuerdos toda la vida, con un toque distinto. nunca está de más tomarse fotos con seres tan importantes en nuestras vidas</p><br><br>
                         </div>                            
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 mb-4">
                         <div class="tm-bg-orange p-5">
-                            <i class="fa fa-4x fa-bell-o d-block mb-4 text-center"></i>
-                            <h3 class="mb-4">Nullam condi mentum</h3>
-                            <p>Mauris fringilla arcu non tellus malesuada tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                            <img src="{{asset('assets/img/7.jpeg')}}" alt="Image" class="img-fluid" width="150" align="text-center" >
+                            <h3 class="mb-4">GRADUACIONES</h3>
+                            <p style=”text-align: justify;” >Realizamos estudios fotográficos profesionales, sesiones personalizadas, creativas, artísticas, a tu gusto, con propuestas únicas y de alta calidad para cubrir tus necesidades. Iluminación controlada, equipo preciso, tiempo, conocimientos, creatividad.</p>
+                            <br><br>
                         </div>                            
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 mb-4">
                         <div class="tm-bg-orange p-5">
-                            <i class="fa fa-4x fa-birthday-cake d-block mb-4 text-center"></i>
-                            <h3 class="mb-4">Nullam condi mentum</h3>
-                            <p>Mauris fringilla arcu non tellus malesuada tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-                        </div>                            
+                            <img src="{{asset('assets/img/6.jpg')}}" alt="Image" class="img-fluid" width="150" align="text-center" >
+                            <h3 class="mb-4">FAMILIAR Y MATERNAL</h3>
+                            <p style=”text-align: justify;” >Queremos que tengas los mejores recuerdos de tu embarazo; por eso nos esforzamos en conseguir las fotografías que deseas, con tu sello personal. Si lo que buscas es algo distinto y creativo estás en el lugar correcto. Podemos hacer para ti y tu familia el mejor recuerdo.</p>
+
                     </div>
-                </div> <!-- row -->
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <div class="tm-box-pad tm-bordered-box">
-                            <h2 class="tm-section-title tm-orange-text">Phasellus vitae finibus lectus</h2>
-                            <p>Praesent finibus egestas metus id pulvinar. Nullam condimentum purus sed accumsan ornare. Pellentesque leo urna, ornare in metus in, fermentum dictum est. Maecenas vehicula velit at enim tempus, et semper sem scelerisque. Sed id sapien venenatis, ultrices velit sit amet, blandit nisi. Mauris ac eros nisl.</p>
-                            <p>Sed at mattis leo, in feugiat sem. Integer a orci neque. Aenean id lorem ut risus maximus interdum. Nunc sodales gravida semper. Mauris sollicitudin lectus imperdiet, cursus mauris ac, aliquet nulla. Integer vitae sodales elit. Morbi rutrum, dui at pharetra gravida, risus felis finibus est, at vestibulum nibh velit sed nisi.</p>
-                        </div>                            
+                </div> <!-- row -->                          
                     </div>
                 </div>
             </div>
