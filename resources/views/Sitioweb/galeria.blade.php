b2763497d6bfee22f5f8a20d9f37540f0d3e9423
Aurelia-cv <al221711718@gmail.com>
2020-07-02 18:49:02 -0500

    galeria

--- resources/views/Sitioweb/galeria.blade.php
+++ resources/views/Sitioweb/galeria.blade.php
@@ -2,22 +2,33 @@
 @section('seccion')
 <div class="tm-main-content tm-gallery-container">                    
                     <div class="grid">                                                    
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-23-01.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-11-01.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-12-01.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-11-02.jpg')}}" alt="Image"></div>                        
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-12-02.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-23-02.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-11-03.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-23-03.jpg')}}" alt="Image"></div>                        
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-11-04.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-11-05.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-12-03.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-12-04.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-11-06.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-23-04.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-11-07.jpg')}}" alt="Image"></div>
-                        <div class="grid-item"><img src="{{asset('assets/img/gallery-img-23-05.jpg')}}" alt="Image"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/img_7.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/img_6.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a13.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a9-01.jpg')}}" alt="Image" width="300"></div>                        
+                        <div class="grid-item"><img src="{{asset('assets/img/a1-02.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a12-01')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a10-01.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a3-03.jpg')}}" alt="Image" width="300"></div>                        
+                        <div class="grid-item"><img src="{{asset('assets/img/a4-02.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/img_5.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a10-02.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/img_8.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a12_02.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a5-02.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/img_2.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/img_3.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/img_4.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a10.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a19.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a5-01.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a7.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a9.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a3-01.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a11-02.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a3-02.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a3-04.jpg')}}" alt="Image" width="300"></div>
+                        <div class="grid-item"><img src="{{asset('assets/img/a1-01.jpg')}}" alt="Image" width="300"></div>
                     </div>                    
                 </div> <!-- tm-main-content -->        
 @endsection
